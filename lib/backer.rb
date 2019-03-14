01b21eea2f4d60e04dcce96f78a84e678fbe805d
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-14 14:28:36 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,9 @@
 class Backer 
- attr_reader :name, :backend_projects 
+ attr_reader :name, :backed_projects 
   
   def initialize(name)
     @name = name
-    @backend_projects = []
+    @backed_projects = []
   end 
   
 end 
