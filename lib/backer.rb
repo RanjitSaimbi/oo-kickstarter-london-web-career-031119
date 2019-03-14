2c2a9791bc2e14e0f8be9db0eb3157db6666bcec
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-14 14:49:20 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -11,7 +11,7 @@ class Backer
   
   def back_project(roject) 
     @backed_projects << project
-    Project.backers  
+    Project.backers << @backed_projects 
     binding.pry 
     "ghdg"
   end 
